a317a5b0a7ba89419d77ba5c71612c01a7f54ed8
sparshjoshi <sparshj567@gmail.com>
2026-01-16 12:35:02 +0530

    leetcode744 commit

--- src/leetcode744.java
+++ src/leetcode744.java
@@ -1,16 +1,16 @@
 class Solution744 {
     public char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
-        int end = letters.length-1;
-        while (start<=end){
-            int mid = start + (end - start)/2;
-            if(letters[mid] == target){
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (letters[mid] == target) {
                 return letters[start];
-            } else if (letters[mid]<target) {
-                start = mid+1;
+            } else if (letters[mid] < target) {
+                start = mid + 1;
 
-            }else {
-                end = mid-1;
+            } else {
+                end = mid - 1;
             }
 
         }
