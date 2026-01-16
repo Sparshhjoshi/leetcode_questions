4cbb5afe78f1c8dbe056ad7d7e8c51f9dfe75eda
sparshjoshi <sparshj567@gmail.com>
2026-01-17 01:26:00 +0530

    leetcode34 commit

--- src/leetcode34.java
+++ src/leetcode34.java
@@ -1,62 +1,38 @@
-import java.util.Arrays;
+class leetcode34 {
 
-class leet34{
     public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
 
-        int first = -1;
-        int last =-1;
-        int [] ans = new int [2];
-        ans [0] = first;
-        ans[1] = last;
+        ans[0] = search34(nums, target, true);   // first position
+        ans[1] = search34(nums, target, false);  // last position
+
+        return ans;
+    }
+
+    public int search34(int[] nums, int target, boolean isFirst) {
         int start = 0;
         int end = nums.length - 1;
+        int ans = -1;
 
-        while(start<=end){
-            int mid = (start + (end-start))/2;
-            if(nums[mid]==target){
-                first = mid;
-                start = first;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
+            if (nums[mid] < target) {
+                start = mid + 1;
             }
-            else if (target>nums[mid]){
-                start = mid;
+            else if (nums[mid] > target) {
+                end = mid - 1;
             }
-            else{
-                end = mid;
+            else {
+                ans = mid;   // store index
+
+                if (isFirst) {
+                    end = mid - 1;    // move left
+                } else {
+                    start = mid + 1;  // move right
+                }
             }
         }
-        while(start<=end){
-            int mid = (start + (end-start))/2;
-            if(nums[mid]==target){
-                last = mid;
-                end = last;
-                break;
-            }
-            else if (target>nums[mid]){
-                start = mid;
-            }
-            else{
-                end = mid;
-            }
-        }
-        System.out.println(Arrays.toString(ans));
-        return ans ;
-    }
-}
-
-public class leetcode34{
-    public static void main(String[] args) {
-        int [] a = {1};
-        int [] b ={};
-        int [] c = {1,1,2,2,3,4,5,6};
-        int [] d ={1,1,2,2,3,3,4,4,5,5,6,6,7,7};
-        leet34 l1 = new leet34();
-        l1.searchRange(a,1);
-        l1.searchRange(a,4);
-        l1.searchRange(b,0);
-        l1.searchRange(c,1);
-        l1.searchRange(d,7);
-        l1.searchRange(d,9);
-
+        return ans;
     }
 }
